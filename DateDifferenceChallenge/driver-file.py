adb3698f0fdcf76245ae47b6d5c7b0d3f32e9abf
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-22 13:52:33 +0800

    file partitioning

---
+++ DateDifferenceChallenge/driver-file.py
@@ -0,0 +1,22 @@
+from DateDifferenceClass import DateDifferenceClass
+from DateDifferenceService import readInput, diffCalculator
+
+# Driver
+print('Welcome to Date Difference Calculator')
+
+print("Enter start date (DD/MM/YYYY): ")
+inputDate = input()
+readInput(inputDate)
+inputDateArr = [ int(x) for x in inputDate.split('/')]
+print(inputDateArr)
+
+print("Enter end date (DD/MM/YYYY): ")
+inputDate = input()
+readInput(inputDate)
+inputDateArr.extend([ int(x) for x in inputDate.split('/')])
+print(inputDateArr)
+
+date1 = DateDifferenceClass(inputDateArr[0], inputDateArr[1], inputDateArr[2])
+date2 = DateDifferenceClass(inputDateArr[3], inputDateArr[4], inputDateArr[5])
+ 
+print(diffCalculator(date1, date2), "days")
