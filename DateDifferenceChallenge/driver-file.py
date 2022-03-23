c4a1f68feb372a2b161139e913945a9981829ee8
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-23 13:48:54 +0800

    added comments

--- DateDifferenceChallenge/driver-file.py
+++ DateDifferenceChallenge/driver-file.py
@@ -6,18 +6,25 @@ from DateDifferenceService import readInput, diffCalculator
 print('Welcome to Date Difference Calculator')
 
 print("Enter start date (DD/MM/YYYY): ")
+# user to input start date
 inputDate = input()
 
+# store validated input date into a placeholder array
 inputDateArr = readInput(inputDate)
+# print out array for debugging purposes
 print(inputDateArr)
 
 print("Enter end date (DD/MM/YYYY): ")
+# user to input end date
 inputDate = input()
 
+# extend validated input date into the existing placeholder array
 inputDateArr.extend(readInput(inputDate))
+# print out array for debugging purposes
 print(inputDateArr)
 
 date1 = DateDifferenceClass(inputDateArr[0], inputDateArr[1], inputDateArr[2])
 date2 = DateDifferenceClass(inputDateArr[3], inputDateArr[4], inputDateArr[5])
- 
+
+# show user the difference in number of days in between the start and end dates
 print(diffCalculator(date1, date2), "days")
