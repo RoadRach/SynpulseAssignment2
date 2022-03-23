19bd9905fe04179391fbe89499f5718f0962bfa4
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-23 13:57:01 +0800

    added comments for DateDifferenceClass and DateDifferenceService

--- DateDifferenceChallenge/DateDifferenceClass.py
+++ DateDifferenceChallenge/DateDifferenceClass.py
@@ -1,3 +1,4 @@
+# date class for creating date objects
 class DateDifferenceClass:
     def __init__(self, dd, mm, yyyy):
         self.dd = dd
--- DateDifferenceChallenge/DateDifferenceService.py
+++ DateDifferenceChallenge/DateDifferenceService.py
@@ -7,36 +7,46 @@ monthDays = [31, 28, 31, 30, 31, 30,
 
 # function for calculating leap years
 def leapYearCalc(d):
+    # extract value of year from object
     years = d.yyyy
+    # calculates the number of leap years that occured
     return int(years / 4) - int(years / 100) + int(years / 400)
 
 # function for calculating the actual number of days between two different dates
 def diffCalculator(date1, date2):
  
-    # count total number of days leading up to date1
+    # sum of days in the years and days of the particular month leading up to date1
     n1 = date1.yyyy * 365 + date1.dd
 
+    # loop for adding up the number of days in the months leading up to the date1's date
     for i in range(0, date1.mm - 1):
         n1 += monthDays[i]
 
+    # add in extra days from leap years that happened leading up to date1
     n1 += leapYearCalc(date1)
 
-    # count total number of days leading up to date2
- 
+    # sum of days in the years and days of the particular month leading up to date2
     n2 = date2.yyyy * 365 + date2.dd
 
+    # loop for adding up the number of days in the months leading up to the date2's date
     for i in range(0, date2.mm - 1):
         n2 += monthDays[i]
 
+    # add in extra days from leap years that happened leading up to date1
     n2 += leapYearCalc(date2)
+
+    # get raw value of the differnece in days between date1 and date2
     valDiff = n2 - n1
 
+    # if start date is later than end date
     if valDiff < 0:
         return abs(valDiff +1)
 
+    # if start date is earlier than end date
     elif valDiff > 0:
         return (valDiff - 1)
 
+    # if both dates are the same
     else:
         return 0
 
